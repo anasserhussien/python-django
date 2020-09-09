916de3d24f685dbec1363336f502fa05b60ee110
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-09 23:27:06 +0200

    add post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 120)
+    slug = models.SlugField(unique = True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __STR__(self):
+        return self.title
+    
