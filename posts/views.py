a8247948f229e1439696fe9a5628f717341af3e9
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-10 23:52:00 +0200

    add comments

--- posts/views.py
+++ posts/views.py
@@ -9,21 +9,25 @@ from rest_framework.generics import (
 from .models import Post
 from .serializers import PostSerializer
 
+
+# List all the posts
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-
+# Get detail of a specific post
 class PostDetailsAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = "slug"
 
+# Update a specific post
 class PostUpdateAPIView(UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = "slug"
 
+# Delete specific posts
 class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
