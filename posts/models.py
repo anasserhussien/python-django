1ec8b99fc3f8729f89c8ff83a40678a84d3338d1
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-13 14:57:19 +0200

    add user field to post

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from django.utils.text import slugify
 # Create your models here.
 
 class Post(models.Model):
-    user = models.models.ForeignKey(settings.AUTH_USER_Model, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length = 120)
     slug = models.SlugField(unique = True)
     content = models.TextField()
