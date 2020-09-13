ddebc8ca69469b723632ce5cb6314ccd8b269f2d
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-13 23:20:45 +0200

    comment details api initialize

--- comments/urls.py
+++ comments/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import *
+
 urlpatterns = [
-    #path('',PostListAPIView.as_view() ),
+    path('<int:pk>/',CommentDetailsAPIView.as_view() ),
 
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import Comment
+from .serializers import *
+from rest_framework.generics import (
+    ListAPIView, 
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+    CreateAPIView
+)
+
+
 # Create your views here.
+
+class CommentDetailsAPIView(RetrieveAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
