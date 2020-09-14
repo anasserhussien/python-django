c05b41841c338a2ecc5497fad81df0aa6a9c4ef9
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-15 01:13:16 +0200

    validate email for user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,25 +5,56 @@ from rest_framework.serializers import(
  SerializerMethodField
 
 )
+
+from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+from django.core.exceptions import ValidationError 
 
 
 class UserCreateSerializer(ModelSerializer):
 
+    # why adding email while it's already added by default?
+    # to make it required field
+    email = serializers.EmailField(
+    label = "Email Adress"
+    )
+    email2 = serializers.EmailField(
+    label = 'Confirm Email'
+    )
     class Meta:
         model = User
         fields = [
             'username',
             'password',
-            'email'
+            'email',
+            'email2'
         ]
 
         extra_kwargs = {"password":
                     {
                         "write_only": True
+                    },
+                    "email2":
+                    {
+                        "write_only": True
                     }}
+
+    def validate(self, data):
+        pass
+        # we can validate anything here also
+
+    def validate_email(self, value):
+
+        data =self.get_initial()
+
+        if User.objects.filter(email = data.get("email")).exists():
+            raise ValidationError("This user is already registered")
+
+        if data.get('email2') == value:
+            return value
+        raise ValidationError("Emails must match")
 
     def create(self, validated_data):
         username = validated_data['username']
