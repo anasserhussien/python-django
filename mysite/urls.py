3683a0ee8213bae27940719484e8a8a6d95b869b
unknown <ahmed_fcih@outlook.com>
2021-02-21 20:31:49 +0200

    add rest_jwt settings urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include("posts.urls")),
     path('comments/', include("comments.urls")),
-    path('accounts/', include("accounts.urls"))
+    path('accounts/', include("accounts.urls")),
+    path('auth/token/', obtain_jwt_token)
 ]
